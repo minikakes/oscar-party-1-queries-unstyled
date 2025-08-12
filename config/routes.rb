432c037c20e8e1a8a7790fb2ec0a0bb6f028ac47
Ben Purinton <ben.purinton@gmail.com>
2025-08-12 15:02:39 -0700

    Directors index RCAV

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
+
+  get("/directors", { :controller => "directors", :action => "index" })
 end
